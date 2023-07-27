9a085d06857ead832579d6f8efe470144b581019
André Moraes <andrelom@gmail.com>
2023-07-27 15:29:52 -0300

    Create getPosts.ts

---
+++ packages/shared/src/data/blog/getPosts.ts
@@ -0,0 +1,21 @@
+import { blog } from '@xc/shared/clients/contentstack'
+import GetAllPosts from './queries/GetAllPosts.graphql'
+
+export type PostsData = {
+  title: string
+  url: string
+}[]
+
+export default async function getPosts() {
+  const response = await blog.gql.query({
+    query: GetAllPosts,
+  })
+
+  if (!response || response.error) {
+    return { ok: false, error: 'Not Found' }
+  }
+
+  const data: PostsData = response.data.all_page_post.items
+
+  return { ok: true, data }
+}

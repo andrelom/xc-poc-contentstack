b3e580fff61d5f0ca79428719de9bd07756245d1
André Moraes <andrelom@gmail.com>
2023-07-27 14:17:13 -0300

    Update getSitemap.ts

--- packages/shared/src/data/blog/getSitemap.ts
+++ packages/shared/src/data/blog/getSitemap.ts
@@ -1,6 +1,13 @@
 import { blog } from '@xc/shared/clients/contentstack'
 import GetSitemapQuery from './queries/GetSitemapQuery.graphql'
 
+const map = ({ items }: { items: Record<string, any>[] }) => {
+  return items.map((item: any) => ({
+    url: item.url ?? '/',
+    updated_at: item.system.updated_at,
+  }))
+}
+
 export default async function getSitemap() {
   const response = await blog.gql.query({
     query: GetSitemapQuery,
@@ -10,5 +17,12 @@ export default async function getSitemap() {
     return { ok: false, error: 'Not Found' }
   }
 
-  return { ok: true, data: {} }
+  return {
+    ok: true,
+    data: {
+      all_page_home: map(response.data.all_page_home),
+      all_page_generic: map(response.data.all_page_generic),
+      all_page_post: map(response.data.all_page_post),
+    },
+  }
 }

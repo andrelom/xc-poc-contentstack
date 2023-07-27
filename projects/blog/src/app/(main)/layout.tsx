8693b1af7777e20718637d53de76c1b9d468f952
André Moraes <andrelom@gmail.com>
2023-07-27 11:44:55 -0300

    Fix layout issues

---
+++ projects/blog/src/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import getRootLayot from '@xc/shared/data/blog/getRootLayout'
+
+import { Root } from '@/layout'
+
+export default async function Layout({ children }: Core.Layout) {
+  const result = await getRootLayot({})
+
+  return (
+    <>
+      <Root data={result.data}>{children}</Root>
+    </>
+  )
+}
--- projects/blog/src/app/layout.tsx
+++ projects/blog/src/app/layout.tsx
@@ -1,17 +1,9 @@
 import './globals.css'
 
-import getRootLayot from '@xc/shared/data/blog/getRootLayout'
-
-import { Root } from '@/layout'
-
 export default async function Layout({ children }: Core.Layout) {
-  const result = await getRootLayot({})
-
   return (
     <html lang="en">
-      <body>
-        <Root data={result.data}>{children}</Root>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }

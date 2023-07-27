6e65286fa3a8239e3c768380d1b1277605f95db0
André Moraes <andrelom@gmail.com>
2023-07-27 18:23:06 -0300

    Update page.tsx

--- projects/blog/src/app/(main)/posts/page.tsx
+++ projects/blog/src/app/(main)/posts/page.tsx
@@ -45,7 +45,7 @@ export default async function Page({}: Core.Page<{ path: string }>) {
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
-            <h2 className="text-base font-semibold leading-7 text-indigo-600">{data.title}</h2>
+            <h2 className="text-base font-semibold leading-7 text-indigo-600">{data.subtitle}</h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{data.title}</p>
             <p className="mt-6 text-lg leading-8 text-gray-600">{data.description}</p>
           </div>

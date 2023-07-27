c1dfe3488316a6e9b2514869368100438a221d48
André Moraes <andrelom@gmail.com>
2023-07-27 10:55:56 -0300

    Update Header.tsx

--- projects/blog/src/layout/Header/Header.tsx
+++ projects/blog/src/layout/Header/Header.tsx
@@ -2,16 +2,25 @@ import type { RootLayoutData } from '@xc/shared/data/blog/getRootLayout'
 
 import Image from 'next/image'
 
+import css from './Header.module.css'
+
 export default function Header({ data }: { data: RootLayoutData | null | undefined }) {
   if (!data) return null
 
   return (
-    <div>
-      <div>
-        <Image src={data.group_header.file_logo.url} alt={data.group_header.file_logo.title} width={180} height={40} />
+    <header className={css.Wrapper}>
+      <div className={css.Container}>
+        <div className={css.Left}>
+          <Image
+            src={data.group_header.file_logo.url}
+            alt={data.group_header.file_logo.title}
+            width={180}
+            height={40}
+          />
+        </div>
+        <div className={css.Center}></div>
+        <div className={css.Right}></div>
       </div>
-      <div></div>
-      <div></div>
-    </div>
+    </header>
   )
 }

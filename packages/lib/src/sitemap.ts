36484dcabf56d1f63a29827285220d55b6435faa
André Moraes <andrelom@gmail.com>
2023-07-27 14:32:57 -0300

    Create sitemap.ts

---
+++ packages/lib/src/sitemap.ts
@@ -0,0 +1,19 @@
+export type SitemapItem = {
+  url: string
+  date: string
+}
+
+export function toSitemapXML(items: SitemapItem[]) {
+  let xml = ''
+
+  xml += '<?xml version="1.0" encoding="utf-8"?>'
+  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+  for (let { url, date } of items) {
+    xml += `<sitemap><loc>${url}</loc><lastmod>${date}</lastmod></sitemap>`
+  }
+
+  xml += '</sitemapindex>'
+
+  return xml
+}

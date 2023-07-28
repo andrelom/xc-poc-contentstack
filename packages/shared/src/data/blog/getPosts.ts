5ab637250fde4a4a355466daf0da59b9021e77eb
André Moraes <andrelom@gmail.com>
2023-07-28 13:57:23 -0300

    Update getPosts.ts

--- packages/shared/src/data/blog/getPosts.ts
+++ packages/shared/src/data/blog/getPosts.ts
@@ -1,24 +1,22 @@
+import logger from '@xc/lib/logger'
 import { blog } from '@xc/shared/clients/contentstack'
-import GetAllPostsQuery from './queries/GetAllPostsQuery.graphql'
 
-export type PostsData = {
+export type PostData = Contentstack.Item<{
   title: string
   url: string
-  system: {
-    created_at: string
-  }
-}[]
+  created_at: string
+}>
 
-export default async function getPosts(): Promise<Core.Result<PostsData>> {
-  const response = await blog.gql.query({
-    query: GetAllPostsQuery,
+export default async function getPosts(): Promise<Core.Result<PostData[]>> {
+  const result = await blog.api.find<PostData>('page_post', (query) => {
+    return query.only(['title', 'url', 'created_at']).toJSON()
   })
 
-  if (!response || response.error) {
-    return { ok: false, error: 'Not Found' }
+  if (!result.ok) {
+    logger.error(result, 'Get Posts')
+
+    return { ok: false, error: result.error }
   }
 
-  const item: PostsData = response.data.all_page_post.items
-
-  return { ok: true, data: item }
+  return { ok: true, data: result.data ?? [] }
 }

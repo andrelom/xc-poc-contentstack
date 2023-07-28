c300433185007a9d56fb95742abca739bfbbcd62
André Moraes <andrelom@gmail.com>
2023-07-28 09:55:44 -0300

    Update getPostsPage.ts

--- packages/shared/src/data/blog/getPostsPage.ts
+++ packages/shared/src/data/blog/getPostsPage.ts
@@ -1,25 +1,43 @@
 import { blog } from '@xc/shared/clients/contentstack'
+import GetAllPostsQuery from './queries/GetAllPostsQuery.graphql'
 
-export type PostsPageData = Contentstack.Item<{
-  subtitle: string
-  description: string
-  open_graph: Contentstack.Globals.OpenGraph
-}>
+export type PostsData = {
+  page: {
+    items: [
+      {
+        title: string
+        subtitle: string
+        description: string
+        open_graph: {
+          og_title: string
+          og_description: string
+        }
+      },
+    ]
+  }
+  posts: {
+    items: [
+      {
+        title: string
+        url: string
+        system: {
+          created_at: string
+        }
+      },
+    ]
+  }
+}
 
-export default async function getPostsPage(): Promise<Core.Result<PostsPageData>> {
-  const result = await blog.api.find<PostsPageData>('page_posts', (query) => {
-    return query.toJSON()
+export default async function getPostsPage(): Promise<Core.Result<PostsData>> {
+  const response = await blog.gql.query({
+    query: GetAllPostsQuery,
   })
 
-  if (!result.ok) {
-    return { ok: false, error: result.error }
-  }
-
-  const item = result.data?.shift()
-
-  if (!item) {
+  if (!response || response.error) {
     return { ok: false, error: 'Not Found' }
   }
+
+  const item: PostsData = response.data.all_page_post.items
 
   return { ok: true, data: item }
 }

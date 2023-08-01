6e1b80051719fb2663b682b43efe0e0d2e5d8f58
André Moraes <andrelom@gmail.com>
2023-08-01 13:56:00 -0300

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -33,7 +33,7 @@ declare namespace NodeJS {
     readonly CONTENTSTACK_LIVE_PREVIEW: string
     readonly CONTENTSTACK_LIVE_EDIT_TAGS: string
     // Application
-    readonly APP_API_TOKEN: string
+    readonly APP_WEBHOOK_TOKEN: string
     readonly APP_PAGE_TYPES: string
   }
 }

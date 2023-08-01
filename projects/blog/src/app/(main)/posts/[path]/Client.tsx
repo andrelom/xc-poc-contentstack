514b568fadc65c49684248ef19fbe811b9c37cb5
André Moraes <andrelom@gmail.com>
2023-08-01 11:24:16 -0300

    Update Client.tsx

--- projects/blog/src/app/(main)/posts/[path]/Client.tsx
+++ projects/blog/src/app/(main)/posts/[path]/Client.tsx
@@ -15,10 +15,12 @@ export default function Client({
 }) {
   return (
     <>
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
-      <div className="prose">
-        <MDXRemote {...data.mdx} />
+      <div className="mx-auto mt-8 max-w-6xl">
+        <h1 className="mb-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{data.title}</h1>
+        <p className="mb-8">{data.description}</p>
+        <div className="prose max-w-full border-t border-t-gray-300 pt-6">
+          <MDXRemote {...data.mdx} />
+        </div>
       </div>
     </>
   )

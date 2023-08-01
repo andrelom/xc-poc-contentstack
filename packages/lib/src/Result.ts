a8eb56d5231c822b01b5ead83c6400f344f273db
André Moraes <andrelom@gmail.com>
2023-08-01 14:59:02 -0300

    Create Result.ts

---
+++ packages/lib/src/Result.ts
@@ -0,0 +1,37 @@
+export default class Result<T = any> {
+  constructor(ok: boolean) {
+    this.ok = ok
+  }
+
+  ok: boolean
+  data?: T
+  error?: string
+  metadata?: Record<string, any>
+
+  static success<T>(data?: T): Result<T> {
+    const result = new Result<T>(true)
+
+    result.data = data
+
+    return result
+  }
+
+  static fail<T>(error: string, metadata?: Record<string, any>): Result<T> {
+    const result = new Result<T>(false)
+
+    result.error = error
+    result.metadata = metadata
+
+    return result
+  }
+
+  static from<T>(source: Record<string, any>): Result<T> {
+    const result = new Result<T>(source.ok)
+
+    result.data = source.data
+    result.error = source.error
+    result.metadata = source.metadata
+
+    return result
+  }
+}

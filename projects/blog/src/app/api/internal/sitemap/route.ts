bdb762be5122a7f6f58cce26d8f412d6e2b89358
André Moraes <andrelom@gmail.com>
2023-08-01 18:15:57 -0300

    Update route.ts

--- projects/blog/src/app/api/internal/sitemap/route.ts
+++ projects/blog/src/app/api/internal/sitemap/route.ts
@@ -15,9 +15,9 @@ export async function GET() {
 
   if (!result.ok) {
     logger.error(result, 'Internal API: Sitemap')
+  } else {
+    logger.info('Internal API: The sitemap was generated')
   }
-
-  logger.info('Internal API: The sitemap was generated')
 
   return new NextResponse(xml, {
     headers: {

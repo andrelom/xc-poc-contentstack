f3d415f779ceca72d4d2de88d331e747554fce71
André Moraes <andrelom@gmail.com>
2023-08-02 11:55:28 -0300

    Create route.ts

---
+++ projects/blog/src/app/api/internal/hooks/revalidate/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import { revalidatePath } from 'next/cache'
+import Result from '@xc/lib/Result'
+import logger from '@xc/lib/logger'
+import getSitemapItems from '@xc/shared/data/blog/getSitemapItems'
+
+export async function POST(request: Request) {
+  const token = request.headers.get('x-webhook-token')
+
+  if (process.env.APP_WEBHOOK_TOKEN !== token) {
+    return NextResponse.json(Result.fail('Unauthorized'), { status: 401 })
+  }
+
+  const result = await getSitemapItems()
+  const items = result.data ?? []
+
+  if (!result.ok) {
+    logger.error(result, 'Internal API: Revalidate')
+  } else {
+    logger.info('Internal API: Triggered static content regeneration')
+  }
+
+  for (const item of items) {
+    revalidatePath(item.url)
+  }
+
+  return NextResponse.json(Result.success())
+}

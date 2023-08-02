1d22da85bcf9294e8b5608984d6937da01f7e3cc
André Moraes <andrelom@gmail.com>
2023-08-01 21:06:25 -0300

    Update klass.ts

--- packages/lib/src/klass.ts
+++ packages/lib/src/klass.ts
@@ -1,16 +1,14 @@
 export type Klass = string | [string, boolean]
 
-export default function klass(css: { readonly [key: string]: string }) {
-  return (...entries: Klass[]) => {
-    return entries.reduce<string>((result, entry) => {
-      if (typeof entry === 'string') {
-        return `${result} ${css[entry]}`
-      }
+export default function klass(css: { readonly [key: string]: string }, ...entries: Klass[]) {
+  return entries.reduce<string>((result, entry) => {
+    if (typeof entry === 'string') {
+      return `${result} ${css[entry]}`
+    }
 
-      const name = entry[0]
-      const show = entry[1]
+    const name = entry[0]
+    const show = entry[1]
 
-      return show ? `${result} ${css[name]}` : result
-    }, '')
-  }
+    return show ? `${result} ${css[name]}` : result
+  }, '')
 }

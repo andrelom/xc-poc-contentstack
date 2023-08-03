536a7f31bc54205d13cb343244ce2da2d92c40db
André Moraes <andrelom@gmail.com>
2023-08-03 16:37:46 -0300

    Update client.ts

--- packages/lib/src/logger/client.ts
+++ packages/lib/src/logger/client.ts
@@ -4,7 +4,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const client = {
   info: (message: string) => cli && dev && console.info(message),
   warn: (message: string) => cli && dev && console.warn(message),
-  error: (obj: any, message: string) => cli && dev && console.error(message, obj),
+  error: (data: any, message?: string) => cli && dev && console.error(data, message),
 }
 
 export default client

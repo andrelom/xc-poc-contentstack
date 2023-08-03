c1b4bb3fb72cea5f5748e796c6ca10fd30f3a7fd
André Moraes <andrelom@gmail.com>
2023-08-02 23:57:12 -0300

    Update layout.tsx

--- projects/blog/src/app/(main)/layout.tsx
+++ projects/blog/src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import getRootLayot from '@xc/shared/data/blog/getRootLayout'
 
+import Contentstack from '@xc/ui/Contentstack'
 import { Root } from '@/layout'
 
 export default async function Layout({ children }: Core.Layout) {
@@ -7,6 +8,7 @@ export default async function Layout({ children }: Core.Layout) {
 
   return (
     <>
+      <Contentstack.LivePreview />
       <Root data={result.data}>{children}</Root>
     </>
   )

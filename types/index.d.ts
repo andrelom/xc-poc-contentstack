2910007d790ae9bc21bbac02fd5392edd457ff67
André Moraes <andrelom@gmail.com>
2023-08-03 17:16:58 -0300

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -24,15 +24,14 @@ declare namespace NodeJS {
     // Contentstack
     readonly CONTENTSTACK_REGION: 'US' | 'EU' | 'AZURE_NA' | 'AZURE_EU'
     readonly CONTENTSTACK_BRANCH: string
-    readonly NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT: string
-    readonly NEXT_PUBLIC_CONTENTSTACK_API_KEY: string
-    readonly NEXT_PUBLIC_CONTENTSTACK_API_HOST: string
     readonly CONTENTSTACK_APP_HOST: string
+    readonly CONTENTSTACK_GRAPHQL_URL: string
     readonly CONTENTSTACK_DELIVERY_TOKEN: string
     readonly CONTENTSTACK_MANAGEMENT_TOKEN: string
     readonly NEXT_PUBLIC_CONTENTSTACK_LIVE_PREVIEW: string
-    readonly NEXT_PUBLIC_CONTENTSTACK_LIVE_EDIT_TAGS: string
-    readonly CONTENTSTACK_GRAPHQL_URL: string
+    readonly NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT: string
+    readonly NEXT_PUBLIC_CONTENTSTACK_API_KEY: string
+    readonly NEXT_PUBLIC_CONTENTSTACK_API_HOST: string
     // Application
     readonly APP_WEBHOOK_TOKEN: string
     readonly APP_PAGE_TYPES: string

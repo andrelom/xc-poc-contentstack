5177f2272d1cf0e3fe5757cf903f54930be9d0b6
André Moraes <andrelom@gmail.com>
2023-08-03 00:02:13 -0300

    Update page.tsx

--- projects/blog/src/app/(main)/posts/[path]/page.tsx
+++ projects/blog/src/app/(main)/posts/[path]/page.tsx
@@ -3,13 +3,10 @@ import { serialize } from 'next-mdx-remote/serialize'
 import { blog } from '@xc/shared/clients/contentstack'
 import createMetadataGenerator from '@xc/lib/createMetadataGenerator'
 import getPostPage from '@xc/shared/data/blog/getPostPage'
-import settings from '@/settings'
 
 import Client from './Client'
 
-export const revalidate = settings.revalidate
-
-export const dynamic = 'force-static'
+export { dynamic, revalidate } from '@/settings'
 
 export const generateMetadata = createMetadataGenerator(({ params }) => {
   return getPostPage({ path: `/posts/${params.path}` })

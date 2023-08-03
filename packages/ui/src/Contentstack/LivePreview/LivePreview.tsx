87976b059d1de5c138164e00294ddfdfdfe5b038
André Moraes <andrelom@gmail.com>
2023-08-03 00:06:05 -0300

    Update LivePreview.tsx

--- packages/ui/src/Contentstack/LivePreview/LivePreview.tsx
+++ packages/ui/src/Contentstack/LivePreview/LivePreview.tsx
@@ -3,10 +3,10 @@
 import { useEffect } from 'react'
 import setLivePreview from './setLivePreview'
 
-export default function LivePreview({}: {}) {
+export default function LivePreview({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     setLivePreview()
   }, [])
 
-  return null
+  return <>{children}</>
 }

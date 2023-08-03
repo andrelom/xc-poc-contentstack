d4c37088eb2e171e21b536de4f1dec7151a2a9ce
André Moraes <andrelom@gmail.com>
2023-08-03 15:42:36 -0300

    Update LivePreview.tsx

--- packages/ui/src/Contentstack/LivePreview/LivePreview.tsx
+++ packages/ui/src/Contentstack/LivePreview/LivePreview.tsx
@@ -5,9 +5,7 @@ const LivePreviewInitializer = dynamic(() => import('./LivePreviewInitializer'))
 const livepreview = process.env.NEXT_PUBLIC_CONTENTSTACK_LIVE_PREVIEW === 'true'
 
 export default function LivePreview({ children }: { children: React.ReactNode }) {
-  if (!livepreview) {
-    return children
-  }
+  if (!livepreview) return children
 
   return <LivePreviewInitializer>{children}</LivePreviewInitializer>
 }

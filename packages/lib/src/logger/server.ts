fc940c26dd770ae5a427542e433ba5173f4f041b
André Moraes <andrelom@gmail.com>
2023-08-03 16:37:48 -0300

    Update server.ts

--- packages/lib/src/logger/server.ts
+++ packages/lib/src/logger/server.ts
@@ -2,9 +2,15 @@ import pino from 'pino'
 
 // Pino is a structured logging package.
 // See more at: https://getpino.io/#/docs/api
-const server = pino({
+const raw = pino({
   name: 'XCentium',
   level: process.env.LOGGING_LEVEL ?? 'info',
 })
+
+const server = {
+  info: (message: string) => raw.info(message),
+  warn: (message: string) => raw.warn(message),
+  error: (data: any, message?: string) => raw.error(data, message),
+}
 
 export default server

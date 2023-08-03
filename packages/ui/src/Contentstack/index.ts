0867fb7680a6d2bbfa65ef1b0311b903b032ae53
André Moraes <andrelom@gmail.com>
2023-08-02 23:57:09 -0300

    Update index.ts

--- packages/ui/src/Contentstack/index.ts
+++ packages/ui/src/Contentstack/index.ts
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic'
 
 const Contentstack = {
   Link: dynamic(() => import('./Link')),
+  LivePreview: dynamic(() => import('./LivePreview')),
 }
 
 export default Contentstack

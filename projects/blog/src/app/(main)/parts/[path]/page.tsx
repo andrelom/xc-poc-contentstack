41178a6d039330cbf3a67fb9b09559708b9f955d
André Moraes <andrelom@gmail.com>
2024-04-26 16:13:08 -0300

    Update page.tsx

--- projects/blog/src/app/(main)/parts/[path]/page.tsx
+++ projects/blog/src/app/(main)/parts/[path]/page.tsx
@@ -2,7 +2,9 @@ import { notFound } from 'next/navigation'
 import createMetadataGenerator from '@xc/lib/createMetadataGenerator'
 import Contentstack from '@xc/ui/Contentstack'
 import getPage from '@xc/shared/data/blog/getPage'
+
 import TeamSection from '@xc/ui/Team'
+
 export { dynamic, revalidate } from '@/ssr'
 
 export const generateMetadata = createMetadataGenerator(({ params }) => {
@@ -12,16 +14,18 @@ export const generateMetadata = createMetadataGenerator(({ params }) => {
 export default async function Page({ params, searchParams }: Core.Page<{ path: string }>) {
   const result = await getPage({ path: `/parts/${params.path}`, preview: searchParams })
 
-  console.log('mmmmmmmmmm', result.data)
   if (!result.ok || !result.data) {
     return notFound()
   }
 
+  const entries = result.data.components.reduce((records, component) => {
+    return [...records, { [component._content_type_uid]: { ...component } }]
+  }, [] as any) as Array<Record<string, unknown>>
 
   return (
     <>
       <Contentstack.ModularBlocks
-        entries={result.data.components}
+        entries={entries}
         components={{
           team: TeamSection,
         }}

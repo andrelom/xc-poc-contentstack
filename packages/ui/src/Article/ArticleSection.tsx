bde56d7c657ba9741361078c1e4c721cd8f6d0b4
Meng Hak <meng.hak@xcentium.com>
2024-04-26 16:09:44 -0700

    .

--- packages/ui/src/Article/ArticleSection.tsx
+++ packages/ui/src/Article/ArticleSection.tsx
@@ -7,16 +7,15 @@ type ArticleProp = {
   data: ArticlePageData
 }
 export default function ArticleSection({ data } : ArticleProp) {
-  console.log('article_section........', data)
   if (!data) return null
 
-  console.log(data,'article')
+  console.log('artcile data........', data)
   return (
     <section>
         <h1 {...tags(data, 'article_title')}>
           {data.article_title}
         </h1>
-        <p className="written-date" {...tags(data, 'article_date')}>Revised{data.article_date}</p>
+        <p className="written-date" {...tags(data, 'article_date')}>Revised {data.article_date}</p>
         <div {...tags(data, 'article_body')} dangerouslySetInnerHTML={{ __html: data?.article_body }} />
     </section>
   )

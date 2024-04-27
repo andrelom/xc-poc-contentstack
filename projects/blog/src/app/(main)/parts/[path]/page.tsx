59f62fb734ff6c184e7ac4de965b849e97f45ab5
André Moraes <andrelom@gmail.com>
2024-04-26 22:38:11 -0300

    Update page.tsx

--- projects/blog/src/app/(main)/parts/[path]/page.tsx
+++ projects/blog/src/app/(main)/parts/[path]/page.tsx
@@ -1,15 +1,9 @@
 import { notFound } from 'next/navigation'
 import createMetadataGenerator from '@xc/lib/createMetadataGenerator'
-import Contentstack from '@xc/ui/Contentstack'
 import getPage from '@xc/shared/data/blog/getPage'
-
-import TeamSection from '@xc/ui/Team'
+import { tags } from '@xc/ui/Contentstack'
 
 export { dynamic, revalidate } from '@/ssr'
-
-export const generateMetadata = createMetadataGenerator(({ params }) => {
-  return getPage({ path: `/parts/${params.path}` })
-})
 
 export default async function Page({ params, searchParams }: Core.Page<{ path: string }>) {
   const result = await getPage({ path: `/parts/${params.path}`, preview: searchParams })
@@ -18,18 +12,15 @@ export default async function Page({ params, searchParams }: Core.Page<{ path: s
     return notFound()
   }
 
-  const entries = result.data.components.reduce((records, component) => {
-    return [...records, { [component._content_type_uid]: { ...component } }]
-  }, [] as any) as Array<Record<string, unknown>>
+  const data = result.data as any
+  const component = result.data.components[0] as any
 
   return (
     <>
-      <Contentstack.ModularBlocks
-        entries={entries}
-        components={{
-          team: TeamSection,
-        }}
-      />
+      <div>
+        <h1 {...(data?.$?.title ?? {})}>This will NOT crash: {data?.title}</h1>
+        <h1 {...(component?.$?.title ?? {})}>This will crash: {component?.title}</h1>
+      </div>
     </>
   )
 }

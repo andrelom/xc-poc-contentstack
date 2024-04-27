a60203d976d0b6bec805679f543d45e787c1b4a3
André Moraes <andrelom@gmail.com>
2024-04-26 23:20:45 -0300

    Update contentstack.ts

--- packages/shared/src/clients/contentstack.ts
+++ packages/shared/src/clients/contentstack.ts
@@ -28,3 +28,32 @@ export const blog = {
     },
   }),
 }
+
+export function createBlogClient() {
+  return {
+    api: createContentstackClient({
+      key: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY,
+      token: process.env.CONTENTSTACK_DELIVERY_TOKEN,
+      environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT,
+      region: process.env.CONTENTSTACK_REGION,
+      branch: process.env.CONTENTSTACK_BRANCH,
+      preview: {
+        enable: process.env.NEXT_PUBLIC_CONTENTSTACK_LIVE_PREVIEW === 'true',
+        host: process.env.NEXT_PUBLIC_CONTENTSTACK_API_HOST,
+        token: process.env.CONTENTSTACK_MANAGEMENT_TOKEN,
+      },
+    }),
+    gql: createApolloClient({
+      url: createURL(process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY, {
+        origin: process.env.CONTENTSTACK_GRAPHQL_URL,
+        search: {
+          environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT,
+        },
+      }),
+      headers: {
+        [`branch`]: process.env.CONTENTSTACK_BRANCH,
+        [`access_token`]: process.env.CONTENTSTACK_DELIVERY_TOKEN,
+      },
+    }),
+  }
+}
